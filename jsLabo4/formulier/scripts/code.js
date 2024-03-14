6509e3b3ecbde89c2fce52b1d1275577bebf0fb5
IlseVer <ilse.verhaeghe@student.vives.be>
2024-03-14 17:10:19 +0100

    js4

--- jsLabo4/formulier/scripts/code.js
+++ jsLabo4/formulier/scripts/code.js
@@ -1,14 +1,30 @@
 const setup = () => {
+    // RANGE
     let range = document.getElementById("range");
     range.addEventListener("input", update);
     range.addEventListener("change", update);
     update();
+
+    //KLEURCODE
+    let color = document.getElementById("color");
+    color.addEventListener("input", updateKleur);
+    color.addEventListener("change", updateKleur);
+    updateKleur();
 }
 
 const update = () => {
     let waardeResults = document.getElementsByClassName("range");
     let resultRange = document.getElementById("resultRange");
     resultRange.innerHTML = waardeResults[0].value;
+}
+
+const updateKleur = () => {
+    let resultKleur = document.getElementsByClassName("color");
+    let kleurHex = document.getElementById("kleurHex");
+    kleurHex.textContent = resultKleur[0].value;
+    kleurHex.style.color = kleurHex.textContent;
+
+
 }
 
 window.addEventListener("load", setup);
